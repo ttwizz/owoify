9facb1ee4f05342722c519ea62aa3b6761fdd471
ttwiz_z <89384561+ttwizz@users.noreply.github.com>
2023-12-28 12:23:24 +0300

    Release 1.1-1

--- src/owoify.lua
+++ src/owoify.lua
@@ -5,10 +5,10 @@ return function(String)
         local Step2 = string.gsub(Step1, "r", "w")
         local Step3 = string.gsub(Step2, "L", "W")
         local Step4 = string.gsub(Step3, "l", "w")
-        local Step5 = string.gsub(Step4, "Ð", "Â")
-        local Step6 = string.gsub(Step5, "ð", "â")
-        local Step7 = string.gsub(Step6, "Ë", "Â")
-        local Step8 = string.gsub(Step7, "ë", "â")
+        local Step5 = string.gsub(Step4, "Ð ", "Ð’")
+        local Step6 = string.gsub(Step5, "Ñ€", "Ð²")
+        local Step7 = string.gsub(Step6, "Ð›", "Ð’")
+        local Step8 = string.gsub(Step7, "Ð»", "Ð²")
         Result = Step8
     end
     return Result
